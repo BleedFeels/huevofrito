306a1629560dde8f1dfd84dbf8ccbbe032dd8401
DargyLobo <dargijuli@gmail.com>
2022-10-28 10:00:41 -0500

    estoy agregando la primera version del huevo frito

---
+++ python.py
@@ -0,0 +1,10 @@
+print("hacer un huevo frito")
+llama=input("quieres cocinar el huevo a llama alta o llama baja?")
+if llama=="llama alta":
+  print("romper la cascara y echar el huevo al sarten")
+  print("esperar cinco minutos de coccion")
+  print("sacarlo del sarten y servirlo")
+elif llama=="llama baja":
+  print("romper la cascara y echar el huevo al sarten")
+  print("esperar ocho minutos de coccion")
+  print("sacarlo del sarten y servirlo")
